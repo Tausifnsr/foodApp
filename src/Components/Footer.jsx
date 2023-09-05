4ba06186e991ecdb7be5c21611cd38fcc5f6618d
Tausifnsr <37775032+Tausifnsr@users.noreply.github.com>
2023-09-06 01:32:44 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         <img className="logoImg" src={Logo} alt="logo" />
       </div>
       <div className="contactUs w-[294px] mt-14 ml-[41px] lg:ml-[442px]">
-        <div className="title text-[15.77px] font-[600] text-[#0E2368] mt-[42px] tracking-wide mb-2 lg:text-[18.84px]">Contact Us</div>
+        <div className="title text-[15.77px] font-[600] text-[#0E2368] mt-[42px] tracking-wide mb-2 lg:mb-4 lg:text-[18.84px]">Contact Us</div>
         <div className="contactItems text-[8.76px] w-[275px] text-[#646874] lg:text-[14.66px]">
           <li className="mb-2">
             Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM
@@ -23,16 +23,16 @@ function Footer() {
         </div>
       </div>
       <div className="more mt-14 ml-[41px] lg:mt-24 lg:ml-[102px]">
-        <div className="title text-[15.77px] font-[600] text-[#0E2368] mb-4 lg:text-[18.84px]">More</div>
+        <div className="title text-[15.77px] font-[600] text-[#0E2368] mb-2 lg:mb-4 lg:text-[18.84px]">More</div>
         <div className="moreItems text-[8.76px] w-[275px] text-[#646874] lg:text-[14.66px]">
-          <li className="mb-2">About Us</li>
-          <li className="mb-2">Products</li>
-          <li className="mb-2">Career</li>
-          <li className="mb-2">Contact Us</li>
+          <li className="mb-2 lg:mb-[14px]">About Us</li>
+          <li className="mb-2 lg:mb-[14px]">Products</li>
+          <li className="mb-2 lg:mb-[14px]">Career</li>
+          <li className="mb-2 lg:mb-[14px]">Contact Us</li>
         </div>
       </div>
       <div className="linkCopy lg:-mt-2 lg:relative lg:mr-16">
-      <div className="copyRight text-[8.76px] w-[111px] text-[#828B9C] mx-auto mt-[30px] lg:text-[14.66px] lg:w-[197px] lg:absolute lg:bottom-[110px] lg:right-[150px] ">© 2022 Food Truck Example</div>
+      <div className="copyRight text-[8.76px] w-[111px] text-[#828B9C] mx-auto mt-[30px] lg:text-[14.66px] lg:w-[197px] lg:absolute lg:bottom-[90px] lg:right-[150px] ">© 2022 Food Truck Example</div>
       <div className="socialLinks lg:relative lg:mt-[105px]">
         <div className="title hidden text-[#0E2368] font-[600] mt-[42px] tracking-wide mb-2 lg:block lg:text-[18.84px]">Social Links</div>
         <div className="socialItems flex mx-36 mt-2 mb-[107px] lg:-ml-4">
